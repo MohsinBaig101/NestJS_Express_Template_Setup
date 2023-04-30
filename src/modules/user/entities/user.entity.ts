0da3bdbd425fd889cfb7e8a8896e5fff930a34aa
Mohsin Baig <mbaigarid@gmail.com>
2023-04-30 22:40:36 +0400

    add the user schema

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+export const userSchema = new Schema({
+    firstName: { type: String, maxlength: 50 },
+    lastName: { type: String, maxlength: 50 },
+    email: { type: String, unique: true },
+    password: { type: String },
+    isActive: { type: Boolean, default: false },
+    emailConfirmed: { type: Boolean, default: false },
+    lastLogin: { type: Date },
+    archived: { type: Date },
+    createdAt: { type: Date, default: new Date },
+    updatedAt: { type: Date, default: new Date }
+});
